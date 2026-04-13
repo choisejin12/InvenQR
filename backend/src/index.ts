14368e3e79efd7faae22f3415ad192c83e2b2ca0
choisejin12 <76937151+choisejin12@users.noreply.github.com>
2026-04-13 19:28:14 +0900

    배포용 코드 수정

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,9 @@ dotenv.config();
 const app = express();
 
 // 미들웨어 설정
-app.use(cors());
+app.use(cors({
+  origin: '*' 
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
