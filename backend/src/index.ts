40dc8263bab28634d44f3ca02f33d01b696a753d
choisejin12 <76937151+choisejin12@users.noreply.github.com>
2026-04-13 19:41:36 +0900

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,9 +16,7 @@ dotenv.config();
 const app = express();
 
 // 미들웨어 설정
-app.use(cors({
-  origin: '*' 
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
